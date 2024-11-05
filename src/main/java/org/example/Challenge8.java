ddd2630ac786d7676b9717fe10413bc82ef8f396
Tawanda Chabva <tchabva@gmail.com>
2024-11-05 14:56:39 +0000

    fix: changed the contailsAll method to the equals

--- src/main/java/org/example/Challenge8.java
+++ src/main/java/org/example/Challenge8.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 public class Challenge8 {
-    BiConsumer<List<Integer>, List<Integer>> compareTwoIntegerLists = (listOne, listTwo) ->{
-        if (listOne.containsAll(listTwo)){
+   public static BiConsumer<List<Integer>, List<Integer>> compareTwoIntegerLists = (listOne, listTwo) ->{
+        if (listOne.equals(listTwo)){
             System.out.println("True");
         } else {
             System.out.println("False");

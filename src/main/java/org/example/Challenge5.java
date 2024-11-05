c1a464c7ae40f8706a5d8ab380de574bd53624bf
Tawanda Chabva <tchabva@gmail.com>
2024-11-05 12:11:35 +0000

    fix: add a new line instruction at the end of the niceGreeting string

--- src/main/java/org/example/Challenge5.java
+++ src/main/java/org/example/Challenge5.java
@@ -5,6 +5,6 @@ import java.util.function.BiConsumer;
 public class Challenge5 {
 
     public static BiConsumer<String, Integer> niceGreeing = (name, age) ->{
-        System.out.printf("Name: %s\nAge: %d\nHello, there I hope you're having a good day", name, age);
+        System.out.printf("Name: %s\nAge: %d\nHello, there I hope you're having a good day\n", name, age);
     };
 }
